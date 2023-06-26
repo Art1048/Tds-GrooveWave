08fd21bf306466002a39e2d0c187436567fea5d5
Arthur Henrique Costa Nunes <arthur.art1048@gmail.com>
2023-06-25 23:47:40 -0300

    fix: regex

--- TDS_GrooveWave/src/GW.Api/OAuth/OAuthService.cs
+++ TDS_GrooveWave/src/GW.Api/OAuth/OAuthService.cs
@@ -9,7 +9,7 @@ public class AuthService
     {
         if(user != null && (user.Email != null || user.Email != "") && (user.Password != null || user.Password != "") && (user.FirstName != null || user.FirstName != "") && (user.LastName != null || user.LastName!= "")){
             if(user.Password!.Length >= 8){
-                Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,6})+)((\.(\w){2,6})+)$");
+                Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,6})+)||^([\w\.\-]+)@([\w\-]+)((\.(\w){2,6})+)((\.(\w){2,6})+)$");
                 Match match = regex.Match(user.Email!);
                 if(match.Success){
                     return true;
